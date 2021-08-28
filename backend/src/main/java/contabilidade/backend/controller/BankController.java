35845af231fef8a13db957fab8903e990f061f2d
Jordevá <jordevalucas@gmail.com>
2021-08-28 16:49:27 -0300

    insert error

--- backend/src/main/java/contabilidade/backend/controller/BankController.java
+++ backend/src/main/java/contabilidade/backend/controller/BankController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BankController {
 
-  private static final Logger log = LoggerFactory.getLogger(
+  // private static final Logger log = LoggerFactory.getLogger(
     BankController.class
   );
   private List<AccountModel> accounts = new ArrayList<>();
